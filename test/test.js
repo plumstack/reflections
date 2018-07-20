be8a06908fd731ef923586a0959a2c993abfc165
Jacob Johnston <johnstonajacob@gmail.com>
2018-07-20 15:37:55 -0500

    test setu[

--- test/test.js
+++ test/test.js
@@ -1,12 +1,31 @@
 /* eslint-disable */
 const chai = require('chai');
+const axios = require('axios');
 
 const expect = chai.expect;
 
-describe('Array', () => {
-  describe('#indexOf()', () => {
-    it('should return -1 when the value is not present', () => {
-      expect([1, 2, 3].indexOf(4)).to.equal(-1);
+describe('restrict unauthorized access to API', function() {
+  describe('/api/auth/checkAuth', function() {
+    let result;
+    before('get results from endpoint', async function() {
+      return new Promise(function(res, rej) {
+        const options = {
+          method: 'GET',
+          url: 'http://localhost:8085/api/auth/checkAuth',
+        };
+        axios(options)
+          .then((response) => {
+            result = response.data;
+            res(result);
+          })
+          .catch(rej);
+      });
+    });
+    it('should return JSON with a loggedIn property', async function() {
+      expect(result).to.have.property('loggedIn');
+    });
+    it('loggedIn property should be false', function() {
+      expect(result.loggedIn).to.equal(false);
     });
   });
 });
