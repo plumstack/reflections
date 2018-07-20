ab44954a512f24d10fcc8504238beb93ba78ed8c
Jacob Johnston <johnstonajacob@gmail.com>
2018-07-19 23:05:44 -0500

    added safety for edge cases of channel names

--- server/database/Slack.js
+++ server/database/Slack.js
@@ -54,9 +54,15 @@ class SlackDB {
 
   async insertNewCohort({ name, archived }) {
     const newCohortSQL = 'INSERT INTO rs.cohorts(id, cohort_status) VALUES ($1, $2) ON CONFLICT (id) DO NOTHING;';
-    const cohortID = parseInt(name.replace('hratx', ''), 10);
+    // removes the hratx prefix and parses the cohort number only if the remains is a number
+    const cohortID = Number(name.replace('hratx', ''));
+
+    // protects against cases where the hratx prefix is used, but not followed by a cohort number
+    // should also be safe against special channels like hratx34resume due to Number constructor
+    if (Number.isNaN(cohortID)) return null;
 
     try {
+      // inserts new cohorts with their numbers as the ID and a status of graduated or archived.
       const newCohortInsert = this.client.query(newCohortSQL, [cohortID, archived ? 'graduated' : 'incoming']);
       return newCohortInsert;
     } catch (error) {
