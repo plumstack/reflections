32a410dc0ac62d79a3c0394d56c2fbe56bad6827
Jacob Johnston <johnstonajacob@gmail.com>
2018-07-26 18:53:57 -0500

    cohort select formatting and router logic

--- src/components/CohortSelect.vue
+++ src/components/CohortSelect.vue
@@ -4,35 +4,35 @@
         <h2>Current</h2>
         <div class="pure-button-group" role="group"
         v-for="cohort in current" :key="cohort.id">
-            <a class='pure-button'>HRATX{{cohort.id}}</a>
+            <CohortSelectItem :cohort='cohort' :status='cohort.cohort_status'/>
         </div>
     </div>
     <div>
       <h2>Incoming</h2>
         <div class="pure-button-group" role="group"
         v-for="cohort in incoming" :key="cohort.id">
-            <a class='pure-button'>HRATX{{cohort.id}}</a>
+            <CohortSelectItem :cohort='cohort' :status='cohort.cohort_status'/>
         </div>
     </div>
     <div>
       <h2>Staff</h2>
         <div class="pure-button-group" role="group"
         v-for="cohort in staff" :key="cohort.id">
-            <a class='pure-button'>Staff</a>
+            <CohortSelectItem :cohort='cohort' :status='cohort.cohort_status'/>
         </div>
     </div>
     <div>
         <h2>Unassigned</h2>
         <div class="pure-button-group" role="group"
         v-for="cohort in unassigned" :key="cohort.id">
-            <a class='pure-button'>Unassigned</a>
+            <CohortSelectItem :cohort='cohort' :status='cohort.cohort_status'/>
         </div>
     </div>
     <div>
         <h2>Graduated</h2>
         <div class="pure-button-group" role="group"
         v-for="cohort in graduated" :key="cohort.id">
-            <a class='pure-button'>HRATX{{cohort.id}}</a>
+            <CohortSelectItem :cohort='cohort' :status='cohort.cohort_status'/>
         </div>
     </div>
 </div>
@@ -40,6 +40,7 @@
 
 <script>
 import axios from 'axios';
+import CohortSelectItem from './CohortSelectItem.vue';
 
 export default {
   name: 'CohortSelect',
@@ -48,7 +49,11 @@ export default {
       cohorts: [],
     };
   },
+  components: {
+    CohortSelectItem,
+  },
   computed: {
+    // TODO redo filtering to be more efficient
     current() { return this.cohorts.filter((cohort) => cohort.cohort_status === 'current'); },
     graduated() { return this.cohorts.filter((cohort) => cohort.cohort_status === 'graduated'); },
     incoming() { return this.cohorts.filter((cohort) => cohort.cohort_status === 'incoming'); },
--- src/components/CohortSelectItem.vue
+++ src/components/CohortSelectItem.vue
@@ -1,5 +1,8 @@
 <template>
 <div>
+  <a v-if='status === "staff"' class='pure-button staff' @click='cohortClicked'>Staff</a>
+  <a v-else-if='status === "unassigned"' class='pure-button'>Unassigned</a>
+  <a v-else class='pure-button'>HRATX{{cohort.id}}</a>
 </div>
 </template>
 
@@ -7,6 +10,13 @@
 
 export default {
   name: 'CohortSelectItem',
+  props: {
+    cohort: Object,
+    status: String,
+  },
+  methods: {
+    cohortClicked() { this.$router.push(`/cohort/${this.cohort.id}`); },
+  },
 };
 
 </script>
