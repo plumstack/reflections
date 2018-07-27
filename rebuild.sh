b020798947b2cb418772a597e03b9d6b8fb14f94
Jacob <jacob@johnstonjacob.com>
2018-07-27 20:54:31 +0000

    deployment setup

--- rebuild.sh
+++ rebuild.sh
@@ -1,10 +1,10 @@
 if [ "$1" == "client" ]; then
 	echo "Rebuilding client files and restarting container..!"
-    npm run build && docker stop reflections && docker rm reflections && docker build -t reflections . && docker run --name reflections -p 8586:8080 -d reflections 
+    npm run build && docker stop reflections && docker rm reflections && docker build -t reflections . && docker run --name reflections --network br0 -d reflections 
 elif [ "$1" == "nocontainer" ]; then
     echo "Starting new container.."
-    docker build -t reflections . && docker run --name reflections -p 8586:8080 -d reflections
+    docker build -t reflections . && docker run --name reflections --network br0 -d reflections
 else
 	echo "Restarting container..!"
-    docker stop reflections && docker rm reflections && docker build -t reflections . && docker run --name reflections -p 8586:8080 -d reflections
+    docker stop reflections && docker rm reflections && docker build -t reflections . && docker run --name reflections --network br0 -d reflections
 fi
